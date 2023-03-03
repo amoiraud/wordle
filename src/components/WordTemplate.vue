d41b7b87a2fb350b88c13c56c98b448a07270ddd
Adrien Moiraud <amoiraud@hotmail.fr>
2023-03-03 20:16:35 +0100

    Update WordTemplate.vue

--- src/components/WordTemplate.vue
+++ src/components/WordTemplate.vue
@@ -1,19 +1,7 @@
 <template>
   <div class="word-template">
-    <div class="letter-input">
-      <span :class="`${lettersClass[0] !== '' ? lettersClass[0] : ''} letter`">{{letters[0] || '&nbsp;'}}</span>
-    </div>
-    <div class="letter-input">
-      <span :class="`${lettersClass[1] !== '' ? lettersClass[1] : ''} letter`">{{letters[1] || '&nbsp;'}}</span>
-    </div>
-    <div class="letter-input">
-      <span :class="`${lettersClass[2] !== '' ? lettersClass[2] : ''} letter`">{{letters[2] || '&nbsp;'}}</span>
-    </div>
-    <div class="letter-input">
-      <span :class="`${lettersClass[3] !== '' ? lettersClass[3] : ''} letter`">{{letters[3] || '&nbsp;'}}</span>
-    </div>
-    <div class="letter-input">
-      <span :class="`${lettersClass[4] !== '' ? lettersClass[4] : ''} letter`">{{letters[4] || '&nbsp;'}}</span>
+    <div class="letter-input" v-for="(letter, key) in lettersClass" :key="key">
+      <span :class="`${lettersClass[key] !== '' ? lettersClass[key] : ''} letter`">{{letters[key] || '&nbsp;'}}</span>
     </div>
   </div>
 </template>
