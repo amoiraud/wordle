ced950a9e7e65a9f87a404b23039198a18d8b9e8
Adrien Moiraud <cac69@live.fr>
2023-03-03 16:44:31 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="header">
+    <div class="infolegale-logo">
+      <router-link to="/">
+        <div class="logo"></div>
+      </router-link>
+    </div>
+  </div>
+  <div class="main-wrapper">
+    <div class="main-container">
+      <RouterView />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<style lang="scss">
+  .header {
+    height: 70px;
+    background-color: #041D3D;
+    color: white;
+  }
+
+  .main-wrapper {
+    background-color: #DDDDDD;
+    padding: 20px;
+    height: calc(100vh - 70px);
+  }
+</style>
